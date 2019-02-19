1c8fd5888389fbcf5681386466a7a7b9d9c177df
Lucy Wilson <lucyleleu@googlemail.com>
2019-02-19 14:40:07 +0000

    added contact page

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function slideOut() {
 
 $(window).on("load",function() {
   $(window).scroll(function() {
-    
+    myFunction();
   var scrollTop = $(this).scrollTop();
 
   $('.header-container').css({
@@ -24,28 +24,12 @@ $(window).on("load",function() {
     
   });
   
-
-
-    
-    
-    
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".fade").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top;
-      
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(800,1);}
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-      }
-    });
+   
   }).scroll(); //invoke scroll-handler on page-load
 });
 
 // When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -56,7 +40,7 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
