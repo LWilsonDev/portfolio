4222f75cc667531cb9b1b770495db13782721596
Lucy Wilson <33698364+LWilsonDev@users.noreply.github.com>
2021-10-09 15:23:37 +0100

    fix contact form and change icon

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,29 +1,27 @@
-function sendMail(contactForm){
-    
-    $(".contact-page-btn").attr('disabled', 'disabled');
-    $(".contact-page-btn").addClass('disabled');
-  
-    emailjs.send('gmail', 'portfolio', {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "message": contactForm.message.value
+function sendMail(contactForm) {
+  $(".contact-page-btn").attr("disabled", "disabled");
+  $(".contact-page-btn").addClass("disabled");
+
+  emailjs
+    .send("service_kuk8msr", "portfolio", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.email.value,
+      message: contactForm.message.value,
     })
     .then(
-        function(response){
-            $(".message").addClass("display");
-            resetFields(contactForm);
-           
-        },
-        function(error) {
-            $(".error-message").addClass("display-error");
-           
-        
-        });
-        return false;  // To block from loading a new page
+      function (response) {
+        $(".message").addClass("display");
+        resetFields(contactForm);
+      },
+      function (error) {
+        $(".error-message").addClass("display-error");
+      }
+    );
+  return false; // To block from loading a new page
 }
 
-function resetFields(contactForm){
-    contactForm.name.value = "";
-    contactForm.email.value = "";
-    contactForm.message.value = "";
+function resetFields(contactForm) {
+  contactForm.name.value = "";
+  contactForm.email.value = "";
+  contactForm.message.value = "";
 }
